63db147f1bc1d7c6a910fa0274af93e87d4a208e
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2024-03-24 18:26:20 +0100

    Initial commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useEffect } from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const Home = () => {
+
+  const isOpen = useStoreModal(state => state.isOpen);
+  const onOpen = useStoreModal(state => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+
+  }, [isOpen, onOpen]);
+
+  return (
+    <main>
+      <div className='p-4'>
+          Children
+      </div>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import { ModalProvider } from "@/providers/modal-provider";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+  description: "Admin Dashboard for Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ModalProvider />
+          {children}
+      </body>
+    </html>
+  );
+}
